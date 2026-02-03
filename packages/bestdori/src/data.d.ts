f7b9de6eda83cd637889bdfff1a9a0a5f7b62282
ahmadyasser72 <163807415+ahmadyasser72@users.noreply.github.com>
2026-02-03 16:08:15 +0800

    be more explicit when using predicted date

--- packages/bestdori/src/data.d.ts
+++ packages/bestdori/src/data.d.ts
@@ -1,3 +1,15 @@
 import type { Data } from "./get-data";
 
 export const { attributes, bands, characters, events, stamps }: Data;
+
+type MapValue<T> = T extends Map<any, infer V> ? V : never;
+
+namespace Bandori {
+	type Attribute = MapValue<typeof attributes>;
+	type Band = MapValue<typeof bands>;
+	type Character = MapValue<typeof characters>;
+	type Event = MapValue<typeof events>;
+	type Stamp = MapValue<typeof stamps>;
+
+	type GameRegion = "jp" | "en";
+}
