17401d731a87d7df065e17d726037ab189f720ca
ahmadyasser72 <163807415+ahmadyasser72@users.noreply.github.com>
2026-02-08 00:41:38 +0800

    display event cards using dropdown

--- packages/bestdori/src/data.d.ts
+++ packages/bestdori/src/data.d.ts
@@ -7,6 +7,7 @@ type MapValue<T> = T extends Map<any, infer V> ? V : never;
 namespace Bandori {
 	type Attribute = MapValue<typeof attributes>;
 	type Band = MapValue<typeof bands>;
+	type Card = MapValue<typeof cards>;
 	type Character = MapValue<typeof characters>;
 	type Event = MapValue<typeof events>;
 	type Stamp = MapValue<typeof stamps>;
