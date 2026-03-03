aeceff066d0e3963cf0441b92e04ad7e6c54325b
ahmadyasser72 <163807415+ahmadyasser72@users.noreply.github.com>
2026-03-03 23:41:44 +0800

    store all page metadata in a single file

---
+++ site/src/lib/metadata.ts
@@ -0,0 +1,29 @@
+interface PageMetadata {
+	title: string;
+	description: string;
+	ogTitle?: string;
+}
+
+export const pages = {
+	index: {
+		title: "Home",
+		description:
+			"A fan site for BanG Dream! Girls Band Party!. Explore game events, stamps, and character birthdays.",
+		ogTitle: "hina-is",
+	},
+	"page/events": {
+		title: "Event Schedule",
+		description:
+			"View upcoming and past event schedules for BanG Dream! Girls Band Party!.",
+	},
+	"page/stamps": {
+		title: "Stamp List",
+		description:
+			"Browse the collection of stamps from BanG Dream! Girls Band Party!, including voiced stamps.",
+	},
+	"page/birthday": {
+		title: "Birthday Tracker",
+		description:
+			"Keep track of upcoming character birthdays in BanG Dream! Girls Band Party!.",
+	},
+} satisfies Record<string, PageMetadata>;
--- site/src/pages/open-graph/[...route].ts
+++ site/src/pages/open-graph/[...route].ts
@@ -1,35 +1,15 @@
 import { OGImageRoute } from "astro-og-canvas";
 
+import { pages } from "~/lib/metadata";
+
 export const prerender = true;
 
 export const { getStaticPaths, GET } = await OGImageRoute({
 	param: "route",
-
-	pages: {
-		index: {
-			title: "hina-is",
-			description:
-				"A fan site for BanG Dream! Girls Band Party!. Explore game events, stamps, and character birthdays.",
-		},
-		"page/events": {
-			title: "Event Schedule",
-			description:
-				"View upcoming and past event schedules for BanG Dream! Girls Band Party!.",
-		},
-		"page/stamps": {
-			title: "Stamp List",
-			description:
-				"Browse the collection of stamps from BanG Dream! Girls Band Party!, including voiced stamps.",
-		},
-		"page/birthday": {
-			title: "Birthday Tracker",
-			description:
-				"Keep track of upcoming character birthdays in BanG Dream! Girls Band Party!.",
-		},
-	},
+	pages,
 
 	getImageOptions: (_path, page) => ({
-		title: page.title,
+		title: "ogTitle" in page ? page.ogTitle : page.title,
 		description: page.description,
 		bgGradient: [[24, 24, 27]],
 		border: { color: [85, 221, 238], width: 20 },
