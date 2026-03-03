2f34ded80ed84567c2feae71e20c83e74b7db41f
ahmadyasser72 <163807415+ahmadyasser72@users.noreply.github.com>
2026-03-03 22:14:41 +0800

    add new page to track characters birthday

--- packages/bestdori/src/schema/characters.ts
+++ packages/bestdori/src/schema/characters.ts
@@ -1,7 +1,24 @@
 import z from "zod";
 
+import { bestdoriJSON } from "..";
 import { Id } from "./constants";
-import { parseRegionTuple } from "./helpers";
+import { dateTimestamp, parseRegionTuple } from "./helpers";
+
+// /api/characters/$id.json
+export const Character = z
+	.object({
+		characterName: z.string().apply(parseRegionTuple),
+		nickname: z.string().nullable().apply(parseRegionTuple),
+		bandId: Id,
+		colorCode: z.templateLiteral(["#", z.hex()]),
+
+		profile: z.object({ birthday: dateTimestamp }),
+	})
+	.transform(({ characterName: name, profile, ...entry }) => ({
+		name,
+		...entry,
+		...profile,
+	}));
 
 // /api/characters/main.3.json
 export const Characters = z
@@ -15,13 +32,27 @@ export const Characters = z
 			colorCode: z.templateLiteral(["#", z.hex()]),
 		}),
 	)
-	.transform((characters) => {
-		const entries = Object.entries(characters).map(
-			([id, { characterName: name, characterType: type, ...entry }]) =>
-				[Number(id), { name, type, ...entry }] as const,
-		);
+	.pipe(
+		z.preprocess(
+			async (characters) => {
+				const entries = await Promise.all(
+					Object.keys(characters).map(
+						async (id) =>
+							[
+								Number(id),
+								await bestdoriJSON<z.input<typeof Character>>(
+									`/api/characters/${id}.json`,
+									true,
+								),
+							] as const,
+					),
+				);
 
-		return new Map(entries);
-	});
+				return new Map(entries);
+			},
+			z.map(Id, Character),
+		),
+	);
 
 export type Characters = z.infer<typeof Characters>;
+export type Character = z.infer<typeof Character>;
