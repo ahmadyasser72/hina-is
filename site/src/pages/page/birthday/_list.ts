3db8a86a2c9dcd1ca48d745673033d562e4f81cd
ahmadyasser72 <163807415+ahmadyasser72@users.noreply.github.com>
2026-03-04 22:46:30 +0800

    add search to birthday tracker

---
+++ site/src/pages/page/birthday/_list.ts
@@ -0,0 +1,39 @@
+import { type Bandori } from "@hina-is/bestdori/data";
+import * as data from "@hina-is/bestdori/data";
+
+import { create, insertMultiple, search } from "@orama/orama";
+import type z from "zod";
+
+import type { schema } from "./_params";
+
+export const filterCharacters = async (
+	{ query }: z.infer<typeof schema>,
+	characters: Bandori.Character[],
+) => {
+	const db = (() => {
+		const characterDB = create({
+			schema: { band: "string", character: "string" },
+		});
+
+		insertMultiple(
+			characterDB,
+			characters.map(({ slug, name, band }) => ({
+				id: slug,
+				character: name,
+				band: band.name,
+			})),
+		);
+
+		return characterDB;
+	})();
+
+	const hits = new Set(
+		(await search(db, { term: query, limit: characters.length })).hits.map(
+			({ id }) => id,
+		),
+	);
+
+	return { filtered: characters.filter(({ slug }) => hits.has(slug)) };
+};
+
+export const getCharacters = () => [...data.characters.values()];
