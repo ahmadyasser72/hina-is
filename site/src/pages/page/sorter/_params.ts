e6f407f461e1721afb1b268361c363b1edea48ed
ahmadyasser72 <163807415+ahmadyasser72@users.noreply.github.com>
2026-03-10 11:03:25 +0800

    add button for continuing sort character

--- site/src/pages/page/sorter/_params.ts
+++ site/src/pages/page/sorter/_params.ts
@@ -2,6 +2,9 @@ import z from "zod";
 
 import { alwaysArray } from "~/lib/schema";
 
-export const schema = z.object({
-	band: z.string().apply(alwaysArray).optional(),
-});
+export const schema = z
+	.object({
+		band: z.string().apply(alwaysArray),
+		continue: z.stringbool(),
+	})
+	.partial();
