8341be56a82e2fad37dffeb59226d10f377cba88
ahmadyasser72 <163807415+ahmadyasser72@users.noreply.github.com>
2026-04-28 16:29:42 +0800

    change default stamp sort to event-release

--- site/src/pages/page/stamps/_params.ts
+++ site/src/pages/page/stamps/_params.ts
@@ -5,7 +5,7 @@ import { alwaysArray } from "~/lib/schema";
 export const schema = z
 	.object({
 		voice_stamp: z.stringbool().catch(false),
-		sort: z.enum(["default", "event-release"]).catch("default"),
+		sort: z.enum(["default", "event-release"]).catch("event-release"),
 		band: z.string().apply(alwaysArray),
 		character: z.string().apply(alwaysArray),
 		query: z.string().toLowerCase().optional(),
